d6bfde1557343ef749e7c39c5bcf62901c056ea5
Dirk Dobson <dirkvancouver@live.com>
2018-09-22 19:57:24 -0600

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_20_221605) do
+ActiveRecord::Schema.define(version: 2018_09_23_015347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "author"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "retirees", force: :cascade do |t|
     t.string "name"
