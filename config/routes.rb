d4f4c96effe0e320c5a01d5b7c5d4cdcfda73cb8
Dirk Dobson <dirkvancouver@live.com>
2018-09-23 17:20:41 -0600

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
-    resources :retirees, only: [:index, :update]
+    resources :retirees, only: [:index, :update] 
+    resources :posts
   end
+ 
 
   #Do not place any routes below this one
   if Rails.env.production?
